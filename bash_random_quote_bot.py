b22ad4ac2b1012b81bdbb6dab03fbc6ef5b02dd5
Qehh <58768263+Qehh@users.noreply.github.com>
2019-12-12 19:45:34 +0300

    Add files via upload

--- bash_random_quote_bot.py
+++ bash_random_quote_bot.py
@@ -1,7 +1,7 @@
-# BashRandomQuoteBot бот для Телеграм, который выдает случайную цитату с bash.im
 import requests
 from bs4 import BeautifulSoup
-import telebot #подключаем библиотеку для работы с ботом
+import telebot
+print('Все библиотеки загружены')
 
 bot = telebot.TeleBot('1023021830:AAF4KuD-FIjgAvLLomcvb5HOxtZWVrVZ0JQ')
  
@@ -10,28 +10,22 @@ from telebot import types
 @bot.message_handler(content_types=['text'])
 
 def get_text_messages(message):
-    keyboard = types.InlineKeyboardMarkup()
-    key_quote = types.InlineKeyboardButton(text='Цитата', callback_data='quote')
-    keyboard.add(key_qoute)
-    bot.send_message(reply_markup=keyboard)
+        bot.send_message(message.from_user.id, "А вот и я, а вот и кнопочка моя: ")
+        keyboard = types.InlineKeyboardMarkup()
+        key_quote = types.InlineKeyboardButton(text='Цитата', callback_data='quote')
+        keyboard.add(key_quote)
+        bot.send_message(message.from_user.id, text='Нажми на кнопку', reply_markup=keyboard)
     
 @bot.callback_query_handler(func=lambda call: True)
 
 def callback_worker(call):
-
-    url = 'https://bash.im/random'    
-    text = get_text(url)
+  
     if call.data == 'quote': 
-        bot.send_message(call.message.chat.id, text)
+        url = 'https://bash.im/random'    
+        #text = get_text(url)
+        rs = requests.get(url)
+        root = BeautifulSoup(rs.content, 'html.parser')
+        article = root.select_one('article')
+        bot.send_message(call.message.chat.id, article.text)
         
-def get_text(url):
-    rs = requests.get(url)
-    root = BeautifulSoup(rs.content, 'html.parser')
-    article = root.select_one('article')
-
-    return article.text
-
-
 bot.polling(none_stop=True, interval=0)
-
-    
